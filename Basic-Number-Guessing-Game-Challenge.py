48eeba639029b9c1c0f56424d00534fb8f76bda6
RascalTwo <therealrascaltwo@gmail.com>
2015-12-11 16:43:20 -0500

    Added basic menu. Allow the changing of the minimum and maximum random number.

--- Basic-Number-Guessing-Game-Challenge.py
+++ Basic-Number-Guessing-Game-Challenge.py
@@ -1,22 +1,56 @@
 import random
 
-attempts = 1
-number = random.randint(1, 100)
+minmax = [1, 100]
+
 while True:
-    guess = raw_input("Guess (1 - 100): ")
-    if guess.isdigit():
-        guess = int(guess)
-        if 1 <= guess and guess <= 100:
-            if guess == number:
-                print "Correct!"
-                print "It Only Took You", attempts, "Attempts!" if attempts > 1 else "Attempt!"
-                break
-            elif guess > number:
-                print "That Guess Is Too High!"
-            elif guess < number:
-                print "That Guess Is Too Low!"
+    print "Choose A Action:"
+    print "1. Play"
+    print "2. Change Minimum & Maximum"
+    print "3. Exit"
+    choice = raw_input("")
+    if choice.isdigit():
+        if choice == "1":
+            attempts = 1
+            number = random.randint(minmax[0], minmax[1])
+            while True:
+                guess = raw_input("Guess (" + str(minmax[0]) + " - " + str(minmax[1]) + "): ")
+                if guess.isdigit():
+                    guess = int(guess)
+                    if guess == number:
+                        print "Correct!"
+                        print "It Only Took You", attempts, "Attempts!" if attempts > 1 else "Attempt!"
+                        break
+                    elif guess > number:
+                        print "That Guess Is Too High!"
+                    elif guess < number:
+                        print "That Guess Is Too Low!"
+                else:
+                    print "That's Not A Number!"
+                attempts += 1
+        elif choice == "2":
+            getting_min_max = True
+            while getting_min_max:
+                minimum = raw_input("Minimum: ")
+                if minimum.isdigit():
+                    minmax[0] = int(minimum)
+                    while True:
+                        maximum = raw_input("Maximum: ")
+                        if maximum.isdigit():
+                            maximum = int(maximum)
+                            if maximum > minmax[0]:
+                                minmax[1] = maximum
+                                getting_min_max = False
+                                break
+                            else:
+                                print "Maximum Given Is Lower Then The Current Minimum(", minimum, ")"
+                        else:
+                            print "Not A Number"
+                else:
+                    print "Not A Number"
+        elif choice == "3":
+            print "Goodbye!"
+            break
         else:
-            print "Guesses Must Be Between 1 And 100!"
+            print "Invalid Option!"
     else:
-        print "That's Not A Number!"
-    attempts += 1
+        print "Not A Number"
