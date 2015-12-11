c0c59a9c5d3aa2d7ed50e8e895f1a3e02a4ae380
RascalTwo <therealrascaltwo@gmail.com>
2015-12-11 16:35:41 -0500

    Verify that the guess is a number, that it is between 1 and 100, and tells the user if the guessed number is too high or low.

--- Basic-Number-Guessing-Game-Challenge.py
+++ Basic-Number-Guessing-Game-Challenge.py
@@ -1,13 +1,22 @@
 import random
 
 attempts = 1
-number = str(random.randint(1, 100))
+number = random.randint(1, 100)
 while True:
-    print number
-    if raw_input("Guess (1 - 100): ") == number:
-        print "Correct!"
-        print "It Only Took You", attempts, "Attempts!" if attempts > 1 else "Attempt!"
-        break
+    guess = raw_input("Guess (1 - 100): ")
+    if guess.isdigit():
+        guess = int(guess)
+        if 1 <= guess and guess <= 100:
+            if guess == number:
+                print "Correct!"
+                print "It Only Took You", attempts, "Attempts!" if attempts > 1 else "Attempt!"
+                break
+            elif guess > number:
+                print "That Guess Is Too High!"
+            elif guess < number:
+                print "That Guess Is Too Low!"
+        else:
+            print "Guesses Must Be Between 1 And 100!"
     else:
-        print "Incorrect, Guess Again!"
-        attempts += 1
+        print "That's Not A Number!"
+    attempts += 1
