71cd70ad709a933f2af0e704979f86dab03702a6
RascalTwo <therealrascaltwo@gmail.com>
2015-12-11 16:30:37 -0500

    Tells the player how many tries it took them to guess the correct number upon doing so.

--- Basic-Number-Guessing-Game-Challenge.py
+++ Basic-Number-Guessing-Game-Challenge.py
@@ -1,9 +1,13 @@
 import random
 
+attempts = 1
 number = str(random.randint(1, 100))
 while True:
+    print number
     if raw_input("Guess (1 - 100): ") == number:
         print "Correct!"
+        print "It Only Took You", attempts, "Attempts!" if attempts > 1 else "Attempt!"
         break
     else:
         print "Incorrect, Guess Again!"
+        attempts += 1
