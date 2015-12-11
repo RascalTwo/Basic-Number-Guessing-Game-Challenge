ad9f767ebf12ab00f511206a62e97f099b9dd07d
RascalTwo <therealrascaltwo@gmail.com>
2015-12-11 16:26:36 -0500

    Allows player to guess the number mutiple times.

--- Basic-Number-Guessing-Game-Challenge.py
+++ Basic-Number-Guessing-Game-Challenge.py
@@ -1,3 +1,9 @@
 import random
 
-print "Correct!" if raw_input("Guess (1 - 100): ") == str(random.randint(1, 100)) else "Incorrect!"
+number = str(random.randint(1, 100))
+while True:
+    if raw_input("Guess (1 - 100): ") == number:
+        print "Correct!"
+        break
+    else:
+        print "Incorrect, Guess Again!"
